25060460efcac3a3110b3299f0160f264b224747
Andrew Ryder <1811535+andrewtryder@users.noreply.github.com>
2023-04-04 15:39:37 -0400

    first commit

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask, jsonify
+import random
+import sqlite3
+
+app = Flask(__name__)
+
+@app.route("/api/random_text")
+def random_text():
+    conn = sqlite3.connect("db/texts.db")
+    cursor = conn.cursor()
+    cursor.execute("SELECT text FROM texts ORDER BY RANDOM() LIMIT 1")
+    random_text = cursor.fetchone()[0]
+    conn.close()
+    return jsonify({"text": random_text})
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ db/texts.sql
@@ -0,0 +1,7 @@
+CREATE TABLE texts (text TEXT);
+INSERT INTO texts (text) VALUES ("Hello, world!");
+INSERT INTO texts (text) VALUES ("Lorem ipsum dolor sit amet, consectetur adipiscing elit.");
+INSERT INTO texts (text) VALUES ("This is a random text.");
+INSERT INTO texts (text) VALUES ("Flask is awesome!");
+INSERT INTO texts (text) VALUES ("Python is my favorite language.");
+INSERT INTO texts (text) VALUES ("The quick brown fox jumps over the lazy dog.");
