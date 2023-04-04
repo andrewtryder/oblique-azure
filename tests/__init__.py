2ca5d6a9f5a6b33d0bbe63b0fd70a4d58eed8745
Andrew Ryder <1811535+andrewtryder@users.noreply.github.com>
2023-04-04 19:32:27 -0400

    Update requirements and tests

---
+++ tests/__init__.py
@@ -0,0 +1,6 @@
+import unittest
+import flask_unittest
+
+def livesuite():
+    from tests.test_app import test_random_text
+    return suite
---
+++ tests/test_app.py
@@ -0,0 +1,19 @@
+import unittest
+import json
+from api.index import app
+
+class TestApp(unittest.TestCase):
+
+    def setUp(self):
+        self.app = app.test_client()
+
+    def test_random_text(self):
+        response = self.app.get('/')
+        self.assertEqual(response.status_code, 200)
+        data = json.loads(response.get_data(as_text=True))
+        self.assertIn("text", data)
+        self.assertIsInstance(data["text"], str)
+        self.assertGreater(len(data["text"]), 0)
+
+if __name__ == '__main__':
+    unittest.main()
